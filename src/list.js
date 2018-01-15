bf1c918e62e3ef4dc844971e248b7ad929fdca04
siobhanpmahoney <siobhan.p.mahoney@gmail.com>
2018-01-15 00:57:02 -0500

    need to add delete button functionality

--- src/list.js
+++ src/list.js
@@ -1,13 +1,83 @@
 /*
 list is responsible for creating a single list component
 */
+
+let store = {lists:[], tasks:[]}
+
 const List = (() => {
   let id = 1
   return class List {
     constructor(title) {
-      //your code here
-      // NOTE: How can we use the private id variable to auto increment list ids🤔?
+      this.title = title
+      this.id = ++id
+      this.taskIds = []
+
+      console.log(this.title)
+      console.log(this.id)
+      console.log(this.taskIds)
+      store.lists.push(this)
+      console.log(store)
     }
   }
 
 })()
+
+
+const newListForm = document.querySelector("form#create-list-form")
+const newListInput = document.querySelector("input[type=text]")
+
+function handleNewListForm(event) {
+  event.preventDefault()
+
+  // creating task list
+  const listTitle = newListInput.value
+
+  const newListDiv = document.createElement("div")
+  newListDiv.className = "list"
+
+// add title
+  const newListTitle = document.createElement("h2")
+  newListTitle.innerText = listTitle
+  newListDiv.appendChild(newListTitle)
+// end title
+
+// add <ul> tags
+const ulTags = document.createElement("ul")
+newListDiv.appendChild(ulTags)
+// end <ul> tags
+
+// create delete button
+const xButton = document.createElement("button")
+xButton.className = "delete-list"
+newListDiv.appendChild(xButton)
+xButton.innerText = "x"
+// end delete button
+
+document.querySelector("#lists").appendChild(newListDiv)
+
+  let newInstance = new List(listTitle)
+  newListDiv.id = `list-${newInstance.id}`
+  newListInput.value = ""
+// end creating task list
+
+
+// send to create-task-object
+  const taskForm = document.querySelector("#parent-list")
+  const newListOption = document.createElement("option")
+  newListOption.innerText = listTitle
+  newListOption.value = newListDiv.id
+  taskForm.add(newListOption)
+// end create-new-task-option
+
+
+
+// add delete list button option
+
+
+
+}
+
+// create handler for deleting list
+
+
+ newListForm.addEventListener("submit", handleNewListForm)
--- src/task.js
+++ src/task.js
@@ -4,9 +4,48 @@ task is responsible for creating a single task object
 const Task = (() => {
   let id = 1
   return class Task {
-    constructor(description, priority) {
-      //your code here
+    constructor(description, priority, list) {
+      this.description = description
+      this.priority = priorty
+      this.id = ++id
+      if(list) {
+        this.listId = list.id
+      }
+      store.tasks.push(this)
+    }
+    setList(list) {
+      this.listId = list.id
     }
   }
 
 })()
+
+
+const newTaskForm = document.querySelector("form#create-task-form")
+const newTaskParent = document.querySelector("#parent-list")
+const newTaskDescrip = document.querySelector("#new-task-description")
+const newTaskPriority = document.querySelector("#new-task-priority")
+
+function handleNewTask(event) {
+  event.preventDefault()
+
+
+  const taskDescrip = newTaskDescrip.value
+  const taskPriority = newTaskPriority.value
+
+  let taskItem = document.createElement("li")
+
+
+  // const listDiv = document.querySelector(`div#list-${newTaskParent.value}`)
+  document.querySelector(`div.list#${newTaskParent.value}`).appendChild(taskItem)
+  console.log(`div.list#${newTaskParent.value}`)
+  console.log(document.querySelector(`div.list#${newTaskParent.value}`))
+  console.log(taskItem)
+  taskItem.innerHTML= `Task: ${taskDescrip}` + `${"\n"}` + `Priority: ${taskPriority}`
+  console.log(newTaskDescrip)
+
+  newTaskDescrip.value = ""
+  newTaskPriority.value = ""
+}
+
+newTaskForm.addEventListener("submit", handleNewTask)
