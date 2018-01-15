ece300c74a59edbd8c645da94610218edcfe9ad2
eal6gf <elum1234@gmail.com>
2018-01-15 13:21:39 -0500

    delete list functionality

--- src/index.js
+++ src/index.js
@@ -3,37 +3,42 @@ document.addEventListener('DOMContentLoaded', () => {
   // your code here ....
 
   //Functions
-  function handleAddList(e){
+  function handleAddList(e) {
     //prevent default form action
     e.preventDefault()
-
     // get text input
     let titleField = document.getElementsByTagName('input')[0].value
+
+    if (titleField !== "") {
     //append to section id lists <div class = 'list'>
     let listsSection = document.getElementById('lists')
-
     //create node
     let newListElement = document.createElement('div')
     newListElement.className += 'list'
-    debugger
+
     //create h2 element to go in node && button
     let newH2 = document.createElement('h2')
-    newH2.innerHTML = `<button data-id="1" class="delete-list">X</button>${titleField}`
+    let newListObj = new List(titleField)
+    newH2.innerHTML = `<button data-id= "${newListObj.id}" class="delete-list">X</button>${titleField}`
+    let deleteButton = newH2.children[0]
+    deleteButton.addEventListener('click', handleDeleteList)
     newListElement.appendChild(newH2)
-
     // append to section
     listsSection.appendChild(newListElement)
-
     //Reset form text
     newListTitleInput.innerText = ''
   }
+
+  }
   // Add Event Listeners
+  function handleDeleteList(e){
+    e.preventDefault()
+    debugger
+    let listParent = e.target.parentNode.parentNode
+    let listsSection = document.getElementById('lists')
+    listsSection.removeChild(listParent)
+  }
+
   let newListTitleInput = document.getElementsByTagName('input')[1]
   newListTitleInput.addEventListener('click', handleAddList)
-
-  // function to create a new list instance and append to dom.
-
-  //function to delete a new list instance and remove from dom?
-
-
 });
