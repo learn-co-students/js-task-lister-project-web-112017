5d4aaf8bb06b0458955fb53f9aeb0a8968d874f3
eal6gf <elum1234@gmail.com>
2018-01-15 00:07:56 -0500

    list functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log("The DOM content has loaded");
   // your code here ....
+
+  //Functions
+  function handleAddList(e){
+    //prevent default form action
+    e.preventDefault()
+
+    // get text input
+    let titleField = document.getElementsByTagName('input')[0].value
+    //append to section id lists <div class = 'list'>
+    let listsSection = document.getElementById('lists')
+
+    //create node
+    let newListElement = document.createElement('div')
+    newListElement.className += 'list'
+    debugger
+    //create h2 element to go in node && button
+    let newH2 = document.createElement('h2')
+    newH2.innerHTML = `<button data-id="1" class="delete-list">X</button>${titleField}`
+    newListElement.appendChild(newH2)
+
+    // append to section
+    listsSection.appendChild(newListElement)
+
+    //Reset form text
+    newListTitleInput.innerText = ''
+  }
+  // Add Event Listeners
+  let newListTitleInput = document.getElementsByTagName('input')[1]
+  newListTitleInput.addEventListener('click', handleAddList)
+
+  // function to create a new list instance and append to dom.
+
+  //function to delete a new list instance and remove from dom?
+
+
 });
--- src/list.js
+++ src/list.js
@@ -5,6 +5,9 @@ const List = (() => {
   let id = 1
   return class List {
     constructor(title) {
+      this.id = ++id;
+      this.title = title;
+      this.tasks = [];
       //your code here
       // NOTE: How can we use the private id variable to auto increment list ids🤔?
     }
